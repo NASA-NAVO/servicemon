1153182fbf62f35f0ce705012fbb2e883792b5a3
Tom Donaldson <tdonaldson@stsci.edu>
2020-08-10 02:46:01 -0400

    Add check for heterogeneous service_type's, logging, and doc updates

---
+++ docs/files/cool_archive_cone_service.py
@@ -0,0 +1,6 @@
+[
+  {'base_name': 'cool_archive_cone',
+   'service_type': 'cone',
+   'access_url': 'http://vo.cool_archive_cone.org/vo/conesearch',
+   }
+]
---
+++ docs/files/sia_service.py
@@ -0,0 +1,9 @@
+[
+    {'base_name': 'SampleSIAService',
+    'service_type': 'xcone',
+    'adql': '',
+    'access_url': 'https://sia.some_archive.org/'
+    'mean.votable?flatten_response=false&sort_by=distance'
+    '&ra={}&dec={}&radius={}'
+    }
+]
