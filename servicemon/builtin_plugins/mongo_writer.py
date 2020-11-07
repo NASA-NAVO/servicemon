72c37ccf9a7d8913d98a1786f5a868c9435b8e1e
JohnGood <jcg@ipac.caltech.edu>
2020-11-07 13:24:22 -0800

    Add mongodb writer.

---
+++ servicemon/builtin_plugins/mongo_writer.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import json
+import requests
+
+from servicemon.plugin_support import AbstractResultWriter
+
+
+class MongoDBResultWriter(AbstractResultWriter, plugin_name='mongo_writer',
+                          description='Sends results to a remote MongoDB database.'):
+
+
+    def begin(self, args, outfile=None):
+
+        self._outURL = 'http://vmnavo01.ipac.caltech.edu/cgi-bin/NAVOMonitor/nph-submit'
+
+        self._logFile = '/tmp/servicemon_' + str(os.getpid()) + '.log'
+
+    def end(self):
+        pass
+
+
+    def one_result(self, stats):
+
+        jsonStr = '{\n'
+
+        fields = stats.columns()
+        self.ncols = len(fields)
+
+        row = stats.row_values()
+
+        jsonStr = json.dumps(row)
+
+        print(jsonStr)
+
+        postData = {'json':jsonStr}
+
+        r = requests.post(url=self._outURL, files=postData)
+
+        with open(self._logFile, 'a+') as file:
+            file.write(r.text)
+            file.close()
+
