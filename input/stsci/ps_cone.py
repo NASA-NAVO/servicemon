7c6f77f17391e82f085c101d0bf632d76628bf86
Tom Donaldson <tdonaldson@stsci.edu>
2021-03-17 21:34:16 -0400

    Make base_name input values more consistent

--- input/stsci/ps_cone.py
+++ input/stsci/ps_cone.py
@@ -1,5 +1,5 @@
 [
-    {'base_name': 'PanSTARRS',
+    {'base_name': 'STScI_PanSTARRS',
      'service_type': 'xcone',
      'adql': '',
      'access_url': 'https://catalogs.mast.stsci.edu/api/v0.1/panstarrs/dr2/'
--- input/stsci/stsci_wise_cone.py
+++ input/stsci/stsci_wise_cone.py
@@ -1,5 +1,5 @@
 [
-    {'base_name': 'WISE_ST',
+    {'base_name': 'STScI_WISE',
      'service_type': 'cone',
      'access_url': 'http://gsss.stsci.edu/webservices/vo/ConeSearch.aspx?CAT=WISE&',
      'adql': ''
